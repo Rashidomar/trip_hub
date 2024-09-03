624361e38fa91adb49e30c10c8eec1c94ac95e7b
Omar <amidurashid@gamil.com>
2024-09-03 06:22:01 -0600

    Feat: Added class validator

--- src/DTO/User.dto.ts
+++ src/DTO/User.dto.ts
@@ -1,6 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, Length,} from 'class-validator';
+
 export class userSignUpDto {
+  @IsNotEmpty()
+  @IsString()
   username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
+ @Length(4,8)
   password: string;
 }
 
