e74ae73cbeebdaa3449b957bbf95061cf699e6cc
Omar <amidurashid@gamil.com>
2024-09-16 22:34:27 -0600

    update 7

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,15 @@ import { UserService } from 'src/user/user.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/user.entity';
 
 @Module({
-  imports: [PassportModule, JwtModule.register({ secret: 'secret' })],
+  imports: [
+    PassportModule,
+    JwtModule.register({ secret: 'secret' }),
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
   providers: [AuthService, UserService, JwtStrategy],
   controllers: [AuthControler],
 })
